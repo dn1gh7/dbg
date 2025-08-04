84ff42d26d49750b70b1180f0e76d5042c859dca
Tilman <tilman.kuehn-velten@protonmail.com>
2025-08-04 11:00:33 +0200

    latest

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,10 +1,20 @@
+import { EventCard } from './eventCard';
+import { EVENTS } from '../globlas';
+
 export default function Home() {
   return (
     <>
       <h1 className="block  text-center h-1/2">
         Willkommen auf der Website der DBG
       </h1>
-      <h1 className="black bg-cambridge text-white p-2">Aktuelles</h1>
+      <h1 className="black bg-cambridge text-white p-2 my-4">Aktuelles</h1>
+      <div className="w-75">{EventCard(EVENTS[2], 0)}</div>
+      <h1 className="black bg-cambridge text-white p-2 my-4">
+        Neueste Publikation
+      </h1>
+      <div>
+        <img className="w-75" src="/publications/9783954771769_g.jpg" alt="" />
+      </div>
     </>
   );
 }
--- src/globlas.ts
+++ src/globlas.ts
@@ -4,8 +4,8 @@ export type Event = {
   text: string;
   imgPaths: string[];
   pdfPath: string;
-  startDate: Date;
-  endDate?: Date;
+  startDate: number;
+  endDate?: number;
 };
 
 export const EVENTS: Event[] = [
@@ -15,8 +15,8 @@ export const EVENTS: Event[] = [
     text: '',
     imgPaths: [''],
     pdfPath: '',
-    startDate: new Date(Date.UTC(2023, 9, 15, 3, 0, 0)),
-    endDate: new Date(Date.UTC(2023, 9, 19, 3, 0, 0)),
+    startDate: Date.UTC(2023, 9, 15, 3, 0, 0),
+    endDate: Date.UTC(2023, 9, 19, 3, 0, 0),
   },
   {
     id: '2',
@@ -24,17 +24,16 @@ export const EVENTS: Event[] = [
     text: '',
     imgPaths: [''],
     pdfPath: '',
-    startDate: new Date(Date.UTC(2024, 3, 5, 3, 0, 0)),
-    endDate: new Date(Date.UTC(2024, 3, 6, 3, 0, 0)),
+    startDate: Date.UTC(2024, 3, 5, 3, 0, 0),
+    endDate: Date.UTC(2024, 3, 6, 3, 0, 0),
   },
-  //9. November 2024
   {
     id: '3',
     title: '9. Deutsch-Bulgarischer Geschichtstag',
     text: 'Am 9. November 2024 fand auf Schloss Heiligenberg in Seeheim-Jugenheim auf Einladung des Deutsch-Bulgarischen Geschichtsvereins Pamet in Zusammenarbeit mit der Deutsch-Bulgarischen Elterninitiative Jan Bibijan und der Deutsch-Bulgarischen Gesellschaft zur Förderung der Beziehungen zwischen Deutschland und Bulgarien e.V. der 9. Deutsch-Bulgarische Geschichtstag statt. Das reichhaltige Programm versprach historische und gegenwartsbezogene brisante Beiträge. Teilnehmende aus Bulgarien, Tschechien, Österreich und Deutschland waren der Einladung gefolgt. Ausführlichere Informationen zur Veranstaltung finden Sie im Bericht zum 9. Deutsch-Bulgarischen Geschichtstag.',
     imgPaths: [''],
     pdfPath: '',
-    startDate: new Date(Date.UTC(2024, 10, 9, 3, 0, 0)),
+    startDate: Date.UTC(2024, 10, 9, 3, 0, 0),
   },
   {
     id: '4',
@@ -43,7 +42,7 @@ export const EVENTS: Event[] = [
     text: '',
     imgPaths: [''],
     pdfPath: '',
-    startDate: new Date(Date.UTC(2023, 5, 28, 3, 0, 0)),
+    startDate: Date.UTC(2023, 5, 28, 3, 0, 0),
   },
   {
     id: '5',
@@ -52,8 +51,8 @@ export const EVENTS: Event[] = [
     text: '',
     imgPaths: [''],
     pdfPath: '',
-    startDate: new Date(Date.UTC(2023, 3, 21, 3, 0, 0)),
-    endDate: new Date(Date.UTC(2023, 3, 22, 3, 0, 0)),
+    startDate: Date.UTC(2023, 3, 21, 3, 0, 0),
+    endDate: Date.UTC(2023, 3, 22, 3, 0, 0),
   },
   {
     id: '6',
@@ -62,7 +61,7 @@ export const EVENTS: Event[] = [
     text: '',
     imgPaths: [''],
     pdfPath: '',
-    startDate: new Date(Date.UTC(2022, 4, 20, 3, 0, 0)),
+    startDate: Date.UTC(2022, 4, 20, 3, 0, 0),
   },
 ];
 
