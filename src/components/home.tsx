450e800bcce5b2c7c51e6226589aab0bf813fcef
Tilman <tilman.kuehn-velten@protonmail.com>
2025-12-05 12:05:50 +0100

    now displaying current events instead of a static one

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,14 +1,33 @@
 import { EventCard } from './eventCard';
-import { EVENTS } from '../globlas';
+import { EVENTS, SocietyEvent } from '../globlas';
+import { useMemo } from 'react';
 
 export default function Home() {
+  const today = new Date();
+
+  const currentEvents = useMemo(() => {
+    const current: SocietyEvent[] = [];
+
+    EVENTS.forEach((event) => {
+      const eventDate = new Date(event.startDate);
+      if (eventDate >= today) {
+        current.push(event);
+      }
+    });
+    current.sort((a, b) => b.startDate - a.startDate);
+
+    return current;
+  }, [EVENTS]);
+
   return (
     <>
       <h1 className="block  text-center h-1/2">
         Willkommen auf der Website der DBG
       </h1>
       <h1 className="black bg-cambridge text-white p-2 my-4">Aktuelles</h1>
-      <div className="w-75">{EventCard(EVENTS[2], 0)}</div>
+      <div className="w-75">
+        {currentEvents.map((event) => EventCard(event, 0))}
+      </div>
       <h1 className="black bg-cambridge text-white p-2 my-4">
         Neueste Publikation
       </h1>
